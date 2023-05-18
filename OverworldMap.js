5ddbbb411789e3f750a7b60b3888ba5a25738dfb
Notwacob <wilson.a.jacob@gmail.com>
2023-05-18 00:26:44 -0400

    Part 1 - 3

--- OverworldMap.js
+++ OverworldMap.js
@@ -20,43 +20,43 @@ class OverworldMap {
 
 window.OverworldMaps = {
     DemoRoom: {
-        lowerSrc: "/images/maps/DemoLower.png",
-        upperSrc: "/images/maps/DemoUpper.png",
+        lowerSrc: "images/maps/DemoLower.png",
+        upperSrc: "images/maps/DemoUpper.png",
         gameObjects: {
             hero: new GameObject({
                 x: 5,
                 y: 6,
-                src: "/images/characters/people/hero.png",
+                src: "images/characters/people/hero.png",
                 useShadow: true
             }),
             npc1: new GameObject({
                 x: 7,
                 y: 9,
-                src: "/images/characters/people/npc1.png",
+                src: "images/characters/people/npc1.png",
                 useShadow: true
             })
         }
     },
     Kitchen: {
-        lowerSrc: "/images/maps/KitchenLower.png",
-        upperSrc: "/images/maps/KitchenUpper.png",
+        lowerSrc: "images/maps/KitchenLower.png",
+        upperSrc: "images/maps/KitchenUpper.png",
         gameObjects: {
             hero: new GameObject({
                 x: 3,
                 y: 5,
-                src: "/images/characters/people/hero.png",
+                src: "images/characters/people/hero.png",
                 useShadow: true
             }),
             npc1: new GameObject({
                 x: 9,
                 y: 6,
-                src: "/images/characters/people/npc1.png",
+                src: "images/characters/people/npc1.png",
                 useShadow: true
             }),
             npc2: new GameObject({
                 x: 10,
                 y: 8,
-                src: "/images/characters/people/npc2.png",
+                src: "images/characters/people/npc2.png",
                 useShadow: true
             }),
         }
