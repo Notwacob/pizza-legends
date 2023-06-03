b93ebf69316fd93a51032f66bbfc8b8e60c52118
Jacob Wilson <wilson.a.jacob@gmail.com>
2023-06-03 19:42:47 -0400

    Pizza RPG Battles Pt. 2 - Attacks and Turns
    Let's add a basic damaging attack to our game. We'll have our pizzas take turns smacking each other in order to demonstrate the core Turn Cycle. You can create more actions in this pattern to add moves to the game!

--- js/Battle/Battle.js
+++ js/Battle/Battle.js
@@ -41,10 +41,10 @@ class Battle {
     this.element.classList.add("Battle");
     this.element.innerHTML = (`
     <div class="Battle_hero">
-        <img src="${"/images/characters/people/hero.png"}" alt="Hero" />
+      <img src="${'/images/characters/people/hero.png'}" alt="Hero" />
     </div>
     <div class="Battle_enemy">
-        <img src=${"/images/characters/people/npc3.png"} alt="Enemy" />
+      <img src=${'/images/characters/people/npc3.png'} alt="Enemy" />
     </div>
     `)
   }
@@ -56,7 +56,21 @@ class Battle {
     Object.keys(this.combatants).forEach(key => {
       let combatant = this.combatants[key];
       combatant.id = key;
-      combatant.init(this.element);
+      combatant.init(this.element)
     })
+
+    this.turnCycle = new TurnCycle({
+      battle: this,
+      onNewEvent: event => {
+        return new Promise(resolve => {
+          const battleEvent = new BattleEvent(event, this)
+          battleEvent.init(resolve);
+        })
+      }
+    })
+    this.turnCycle.init();
+
+
   }
+
 }
--- js/RevealingText.js
+++ js/RevealingText.js
@@ -1,51 +1,52 @@
 class RevealingText {
-    constructor(config) {
-        this.element = config.element;
-        this.text = config.text;
-        this.speed = config.speed || 70;
+  constructor(config) {
+    this.element = config.element;
+    this.text = config.text;
+    this.speed = config.speed || 60;
 
-        this.timeout = null;
-        this.isDone = false;
+    this.timeout = null;
+    this.isDone = false;
+  }
+
+  revealOneCharacter(list) {
+    const next = list.splice(0,1)[0];
+    next.span.classList.add("revealed");
+
+    if (list.length > 0) {
+      this.timeout = setTimeout(() => {
+        this.revealOneCharacter(list)
+      }, next.delayAfter)
+    } else {
+      this.isDone = true;
     }
+  }
 
-    revealOneCharacter(list) {
-        const next = list.splice(0,1)[0];
-        next.span.classList.add("revealed");
+  warpToDone() {
+    clearTimeout(this.timeout);
+    this.isDone = true;
+    this.element.querySelectorAll("span").forEach(s => {
+      s.classList.add("revealed");
+    })
+  }
 
-        if (list.length > 0) {
-            this.timeout = setTimeout(() => {
-                this.revealOneCharacter(list);
-            }, next.delayAfter)
-        } else {
-            this.isDone = true;
-        }
-    }
+  init() {
+    let characters = [];
+    this.text.split("").forEach(character => {
 
-    warpToDone() {
-        clearTimeout(this.timeout);
-        this.isDone = true;
-        this.element.querySelectorAll("span").forEach(s => {
-            s.classList.add("revealed");
-        })
-    }
+      //Create each span, add to element in DOM
+      let span = document.createElement("span");
+      span.textContent = character;
+      this.element.appendChild(span);
 
-    init() {
-        let characters = [];
-        this.text.split("").forEach(character => {
+      //Add this span to our internal state Array
+      characters.push({
+        span,
+        delayAfter: character === " " ? 0 : this.speed         
+      })
+    })
 
-            // Create each span, add o element in DOM
-            let span = document.createElement("span");
-            span.textContent = character;
-            this.element.appendChild(span);
+    this.revealOneCharacter(characters);
 
-            // Add this span to our internal state Array
-            characters.push({
-                span,
-                delayAfter: character === " " ? 0 : this.speed
-            })
-        });
-
-        this.revealOneCharacter(characters);
-    }
+  }
 
 }
